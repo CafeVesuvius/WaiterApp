fd5f73fa0cf441f06641ea4e4fad70fa8bff68c7
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-22 10:28:03 +0200

    Made a very basic "notes" function for orders.

--- MenuKortV1/ViewModel/OrdreViewModel.cs
+++ MenuKortV1/ViewModel/OrdreViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using MenuItem = MenuKortV1.Model.MenuItem;
 
 namespace MenuKortV1.ViewModel
@@ -9,15 +12,58 @@ namespace MenuKortV1.ViewModel
 
     public partial class OrdreViewModel : ObservableObject
     {
-        //public OrdreViewModel()
-        //{
-        //}
+        //
+        public OrdreViewModel()
+        {
+        }
 
         //Define observable collection for the current order
         [ObservableProperty]
         ObservableCollection<MenuItem> order = new ObservableCollection<MenuItem>();
 
         [ObservableProperty]
-        string fullPrice;
+        string note = "";
+
+        [ObservableProperty]
+        Editor thisEditor = new Editor();
+
+        [ObservableProperty]
+        bool noteIsEnabled = false;
+
+        [ObservableProperty]
+        string dynamicButtonName = "Noter til ordre";
+
+        [RelayCommand]
+        void ResetOrder()
+        {
+            Order.Clear();
+        }
+
+        [RelayCommand]
+        void RemoveThisItemFromOrder(MenuItem mi)
+        {
+            Order.Remove(mi);
+        }
+
+        [RelayCommand]
+        void ShowHideNoteField(Editor e)
+        {
+            //e.Text = "cringe";
+            //Note = "";
+            switch (NoteIsEnabled)
+            {
+                case true:
+                    NoteIsEnabled = false;
+                    DynamicButtonName = "Noter til ordre";
+                    break;
+
+                case false:
+                    NoteIsEnabled = true;
+                    DynamicButtonName = "Gem";
+                    break;
+            }
+        }
+
+
     }
 }
