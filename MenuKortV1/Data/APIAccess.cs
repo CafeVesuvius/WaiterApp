8363ffa0abb6a4fd2c1a8cce8f9d5a3c1fb8b94c
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-26 13:00:51 +0200

    Begin working on POST Order feature.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

--- MenuKortV1/Data/APIAccess.cs
+++ MenuKortV1/Data/APIAccess.cs
@@ -1,6 +1,10 @@
 ﻿using MenuKortV1.Model;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 
 namespace MenuKortV1.Data
 {
@@ -9,32 +13,76 @@ namespace MenuKortV1.Data
         // Define a variable to store the API adress
         static readonly string ApiBaseUrl = "http://10.130.54.74:2000";
 
+        //key string
+        static readonly string Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJDVl9BZG1pbiIsIm5iZiI6MTY5NTcxODUyMSwiZXhwIjoxNjk1ODA0OTIxLCJpYXQiOjE2OTU3MTg1MjF9.d8aAnxU5nZNj717X-IzDfkXP7q9a27I1NamPMdfcsko";
+
         // Define API token
-        private static string AuthorizationToken = "test";
+        private static readonly string AuthorizationToken = Key;
 
         // Define a http client
-        static readonly HttpClient client;
+        static HttpClient Client = new HttpClient();
+
+        // Define serializer
+        static JsonSerializerOptions serializerOptions;
 
         // Set up the http connection to the API
         static APIAccess()
         {
-            client = new HttpClient() { BaseAddress = new Uri(ApiBaseUrl) };
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-            client.DefaultRequestHeaders.Add("dotnetconfstudentzone", AuthorizationToken);
+            Client.DefaultRequestHeaders.Add("Authorization", AuthorizationToken);
+            Client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
         }
+
+        // http response
+        static HttpResponseMessage APIResponse;
 
         // Get json string and deserialize it
         public static async Task<Menu> GetMenu()
         {
-            var json = await client.GetStringAsync(ApiBaseUrl+"/api/menu");
-            var menus = JsonConvert.DeserializeObject<Menu>(json);
-            return menus;
+            try
+            {
+                APIResponse = await Client.GetAsync($"{ApiBaseUrl}/api/menu");
+                APIResponse.EnsureSuccessStatusCode();
+                string responseBody = await APIResponse.Content.ReadAsStringAsync();
+                Menu menus = JsonConvert.DeserializeObject<Menu>(responseBody);
+                return menus;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         // Serialise
-        public static async Task<Order> OrderPoster()
+        public static async Task<string> OrderPoster(Order o)
         {
-            return null;
+            try
+            {
+                string json = System.Text.Json.JsonSerializer.Serialize<Order>(o, serializerOptions);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                APIResponse = await Client.PostAsync($"{ApiBaseUrl}/api/order/all", content);
+
+                return ":3";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+            //var msg = new HttpRequestMessage(HttpMethod.Post, $"{ApiBaseUrl}/api/orders");
+            //msg.Content = JsonContent.Create<Order>(o);
+            //var response = await Client.SendAsync(msg);
+            //response.EnsureSuccessStatusCode();
+            //var itemJson = await response.Content.ReadAsStringAsync();
+            //var insertedOrder = System.Text.Json.JsonSerializer.Deserialize<Order>(itemJson);
+            //return insertedOrder;
         }
     }
 }
--- MenuKortV1/ViewModel/OrdrePageViewModel.cs
+++ MenuKortV1/ViewModel/OrdrePageViewModel.cs
@@ -1,8 +1,16 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Platform;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MenuKortV1.Data;
+using MenuKortV1.Model;
+using MenuKortV1.View;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +18,72 @@ namespace MenuKortV1.ViewModel
 {
     public partial class OrdrePageViewModel : ObservableObject
     {
-
-        public Entry ThisEntry { get; set; } = new Entry();
-
-        [RelayCommand]
-        void PostOrder()
+        public OrdrePageViewModel() 
         {
 
         }
 
+        // Define OP string for storing the order name
+        [ObservableProperty]
+        private string orderName;
+
+        // Define OP bool for the entry's "IsEnabled"
+        [ObservableProperty]
+        bool entryEnablerDisabler = true;
+
+        [ObservableProperty]
+        Order myOrder;
+
+        // Function to post order
+        [RelayCommand]
+        async Task PostOrderAsync()
+        {
+            // Soft Keyboard Showe/Hide Trick
+            EntryEnablerDisabler = false;
+            EntryEnablerDisabler = true;
+
+            // Check if the order name field is not empty
+            if (!string.IsNullOrEmpty(OrderName))
+
+            {
+                // If the order name isn't empty, create the order
+                MyOrder = new Order()
+                {
+                    Name = OrderName,
+                    CreatedDate = DateTime.Now,
+                    IsCompleted = false,
+                    OrderLines = new List<OrderLine>() { }
+                };
+
+                // Post order to API & get response
+                var boing = await APIAccess.OrderPoster(MyOrder);
+
+                AToastToYou(boing);
+
+                // If there is response -> continue
+                //if (boing == ":3")
+                //{
+                //    await Shell.Current.GoToAsync(nameof(OrdreDetailsPage));
+                //}
+                //else
+                //{
+                //    AToastToYou(boing);
+                //}
+                
+            }
+            else
+            {
+                // If the order name field is empty, show this popup
+                AToastToYou("Du mangler at indtaste ordre navn.");
+            }
+        }
+
+        // Dynamisk "Toast" (popup) function
+        public static Task AToastToYou(string toastText)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var yourToast = Toast.Make(toastText, ToastDuration.Short, 14).Show(cts.Token);
+            return yourToast;
+        }
     }
 }
