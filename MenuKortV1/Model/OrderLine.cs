1cca65e103cd70aa8761b061a5d05630d671daf3
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-26 07:55:09 +0200

    Create a model for "OrderLine"
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/Model/OrderLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.Model
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+
+        public byte Quantity { get; set; }
+
+        public string Detail { get; set; } = null!;
+
+        public int MenuItemID { get; set; }
+
+        public int OrderID { get; set; }
+    }
+}
