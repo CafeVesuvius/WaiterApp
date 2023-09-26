4993f31e5655c64bd78d832b21058ff51b3d1ceb
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-26 07:54:52 +0200

    Create a model for "Order"
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/Model/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public DateTime? CreatedDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public virtual ICollection<OrderLine> OrderLines { get; set; } = new List<OrderLine>();
+    }
+}
