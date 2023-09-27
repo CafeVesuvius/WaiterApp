96c7fdf74176c510e6ca0788c8054ab5e6d26f74
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-27 09:56:12 +0200

    Assign JSON Properties to the model variables.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

--- MenuKortV1/Model/Order.cs
+++ MenuKortV1/Model/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,19 @@ namespace MenuKortV1.Model
 {
     public class Order
     {
+        //[JsonProperty("id")]
         //public int Id { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; } = null!;
 
+        [JsonProperty("createdDate")]
         public DateTime? CreatedDate { get; set; }
 
+        [JsonProperty("isCompleted")]
         public bool IsCompleted { get; set; }
 
+        [JsonProperty("orderLines")]
         public virtual ICollection<OrderLine> OrderLines { get; set; } = new List<OrderLine>();
     }
 }
--- MenuKortV1/Model/OrderLine.cs
+++ MenuKortV1/Model/OrderLine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,13 @@ namespace MenuKortV1.Model
 {
     public class OrderLine
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("quantity")]
         public byte Quantity { get; set; }
 
+        [JsonProperty("detail")]
         public string Detail { get; set; } = null!;
 
         public int MenuItemID { get; set; }
