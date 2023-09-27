2e2024989578ab29535599b4ee37cee4d23e8dd6
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-27 12:22:27 +0200

    Combine the "OrdreViewModel" and the "OrdreDetailsViewModel" into the "MainViewModel"
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

--- MenuKortV1/ViewModel/MainViewModel.cs
+++ MenuKortV1/ViewModel/MainViewModel.cs
@@ -6,62 +6,141 @@ using MenuKortV1.Data;
 using MenuKortV1.Model;
 using MenuKortV1.View;
 using System.Collections.ObjectModel;
+using MenuItem = MenuKortV1.Model.MenuItem;
 using ObservableObject = CommunityToolkit.Mvvm.ComponentModel.ObservableObject;
 
 namespace MenuKortV1.ViewModel
 {
+    [QueryProperty("OrderLines", "OrderLines")]
     public partial class MainViewModel : ObservableObject
     {
-        // Define an observable collection for menus
-        public ObservableCollection<Menu> Menus { get; set; } = new ObservableCollection<Menu>();
-
-        // Define bool for Show&Hide behaviour for home screen title
-        [ObservableProperty]
-        bool showTitle = false;
-
-        // Pass menu parameters to a new page
-        [RelayCommand]
-        async Task OpenMenu(Menu m)
+        //
+        public MainViewModel()
         {
-            await Shell.Current.GoToAsync($"{nameof(MenuItemPage)}?", new Dictionary<string, object> { { "Menu" , m } } );
         }
 
-        // Command, which gets/refreshes menu data from the API
+        // Define OP for a dymanic page title.
+        [ObservableProperty]
+        private string pageTitle = "Ordre";
+
+        // Define OP string for storing the order name
+        [ObservableProperty]
+        private string orderName;
+
+        // Define OP bool for the entry's "IsEnabled"
+        [ObservableProperty]
+        bool entryEnablerDisabler = true;
+
+        // Define "Order" object
+        [ObservableProperty]
+        Order myOrder;
+
+        // Define observable collection for the current order's order lines
+        [ObservableProperty]
+        ObservableCollection<MenuItem> orderLines = new ObservableCollection<MenuItem>();
+
+        //
+        [ObservableProperty]
+        bool showOrderLinesManager = false;
+
+        //
+        [ObservableProperty]
+        bool showOrderCreationMenu = true;
+
+        // Function to open new order
         [RelayCommand]
-        async Task Refresh()
+        void OpenNewOrder()
         {
-            // Get data
-            var menus = await APIAccess.GetMenu();
+            // Soft Keyboard Showe/Hide Trick
+            EntryEnablerDisabler = false;
+            EntryEnablerDisabler = true;
 
-            // Check if data isn't null
-            if(menus != null)
+            // Check if the order name field is not empty
+            if (!string.IsNullOrEmpty(OrderName))
+
             {
-                // Clear the menu list before refreshing it with new data
-                Menus.Clear();
-
-                // Only add active menus to the menu list
-                if(!menus.Active)
+                // If the order name isn't empty, create the order
+                MyOrder = new Order()
                 {
-                    // Remove inactive menu items from the menu
-                    foreach (var menuItem in menus.Items)
-                    {
-                        if (!menuItem.Active)
-                        {
-                            menus.Items.Remove(menuItem);
-                        }
-                    }
+                    Name = OrderName,
+                    CreatedDate = DateTime.Now,
+                    IsCompleted = false,
+                    OrderLines = new List<OrderLine>() { }
+                };
 
-                    // Add the menus
-                    Menus.Add(menus);
-                }
+                ShowOrderLinesManager = true;
+                ShowOrderCreationMenu = false;
 
-                // Only show "Vælg er menu fra listen:" if there is at least one menu in the list
-                ShowTitle = true;
+                PageTitle = MyOrder.Name;
             }
             else
             {
-                AToastToYou("NO MENUUUUS!!");
+                // If the order name field is empty, show this popup
+                AToastToYou("Du mangler at indtaste ordre navn.");
             }
+        }
+
+        [RelayCommand]
+        async Task OpenMenuList()
+        {
+            await Shell.Current.GoToAsync($"{nameof(MenusPage)}?", new Dictionary<string, object> { { "MyOrder", MyOrder }, { "OrderLines", OrderLines } });
+        }
+
+        // Define a string variable to store notes for the order
+        [ObservableProperty]
+        string note = "";
+
+        // Define editor/text field for order notes
+        public Editor ThisEditor { get; set; } = new Editor();
+
+        // Define a command, which clears an order
+        [RelayCommand]
+        void ResetOrder()
+        {
+            OrderLines.Clear();
+        }
+
+        // Define a command, which removes a specific item from an order
+        [RelayCommand]
+        void RemoveThisItemFromOrder(MenuItem mi)
+        {
+            OrderLines.Remove(mi);
+        }
+
+        // Send the order to API/kitchen
+        [RelayCommand]
+        async Task SendOrdre()
+        {
+            //Note = ThisEditor.Text;
+
+            //foreach (MenuItem mi in Order)
+            //{
+            //    OrderLine ol = new OrderLine
+            //    {
+            //        Id = MyOrder.OrderLines.Count + 1,
+            //        Quantity = 1,
+            //        Detail = ThisEditor.Text,
+            //        MenuItemID = mi.Id
+            //    };
+
+            //    MyOrder.OrderLines.Add(ol);
+            //}
+
+            //Post order to API & get response
+            //var orderPosterResponse = await APIAccess.OrderPoster(MyOrder);
+
+            //if (orderPosterResponse)
+            //{
+            //    Order.Clear();
+            //    Order = null;
+            //    MyOrder = null;
+            //    await Shell.Current.GoToAsync(nameof(OrdrePage));
+            //}
+            //else
+            //{
+            //    MainViewModel.AToastToYou("ERROR");
+            //}
+
         }
 
         // Dynamisk "Toast" (popup) function
@@ -71,5 +150,6 @@ namespace MenuKortV1.ViewModel
             var yourToast = Toast.Make(toastText, ToastDuration.Short, 14).Show(cts.Token);
             return yourToast;
         }
+
     }
 }
