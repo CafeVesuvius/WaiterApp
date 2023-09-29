43e86d3eaba66738f9b02748cc5c9fbbeb2a641a
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-29 11:17:48 +0200

    Model to store persistent data about orders.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/Model/PersistentOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.Model
+{
+    public class PersistentOrder
+    {
+        public List<Order> PersistentOrders { get; set; }
+    }
+}
