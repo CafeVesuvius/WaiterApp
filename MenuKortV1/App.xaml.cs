ec31ad7b9ad31bcbe85fab26b69d9cf0286538c2
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-09-29 11:18:33 +0200

    define the persistent data in the app file
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

--- MenuKortV1/App.xaml.cs
+++ MenuKortV1/App.xaml.cs
@@ -1,7 +1,13 @@
-﻿namespace MenuKortV1
+﻿using MenuKortV1.Model;
+
+namespace MenuKortV1
 {
     public partial class App : Application
     {
+        // Define persistent data
+        public static UserLoginInfo UserLoginInfo;
+        public static PersistentOrder PersistentOrder;
+
         public App()
         { 
             InitializeComponent();
