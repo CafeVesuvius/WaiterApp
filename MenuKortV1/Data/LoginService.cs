413616a7eed33be408461f986fc0c8470f95593c
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-10-10 13:18:56 +0200

    Create Authentication function.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/Data/LoginService.cs
@@ -0,0 +1,32 @@
+﻿using MenuKortV1.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.Data
+{
+    public class LoginService : ILoginService
+    {
+        public async Task<LoginResponse> Authenticate(LoginRequest loginRequest)
+        {
+            using (var client = new HttpClient())
+            {
+                string loginRequestJson = JsonConvert.SerializeObject(loginRequest);
+                var response = client.PostAsync("http://10.130.54.74:2000/api/Authentication", new StringContent(loginRequestJson, Encoding.UTF8, "application/json"));
+
+                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var json = await response.Result.Content.ReadAsStringAsync();
+                    return new LoginResponse
+                    {
+                        Token = json,
+                    };
+                }
+                else { return null; }
+            }
+        }
+    }
+}
