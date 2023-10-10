7eafa4fc3e93a80010d0e18db82d7020beb10256
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-10-10 13:19:56 +0200

    Create a model for storing the JWT token and login data.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/Model/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.Model
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+}
