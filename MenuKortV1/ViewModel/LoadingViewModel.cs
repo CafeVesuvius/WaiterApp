cb0448f4a54774a1be8b5db49356342d4800c192
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-10-10 13:32:10 +0200

    Create view model for the loading screen. It checks whether the authorisation token/login info have expired or not.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/ViewModel/LoadingViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MenuKortV1.Data;
+using MenuKortV1.Model;
+using MenuKortV1.View;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.ViewModel
+{
+    public partial class LoadingViewModel : ObservableObject
+    {
+        public LoadingViewModel()
+        {
+            CheckUserLoginDetails();
+        }
+
+        async void CheckUserLoginDetails()
+        {
+            string userLoginInfo = Preferences.Get(nameof(App.UserLoginInfo), "");
+
+            if (string.IsNullOrWhiteSpace(userLoginInfo))
+            {
+                await Shell.Current.GoToAsync(nameof(LoginPage));
+            }
+            else
+            {
+                var tokenDetail = await SecureStorage.GetAsync(nameof(App.Token));
+
+                var handler = new JwtSecurityTokenHandler();
+                var jsonToken = handler.ReadToken(tokenDetail) as JwtSecurityToken;
+
+                if (jsonToken.ValidTo < DateTime.UtcNow)
+                {
+                    //await CustomCommands.AToastToYou("Token")
+                    await Shell.Current.DisplayAlert("User session expired", "Login again to continue", "Ok");
+                    await Shell.Current.GoToAsync(nameof(LoginPage));
+                }
+                else
+                {
+                    var userInfo = JsonConvert.DeserializeObject<UserLoginInfo>(userLoginInfo);
+                    App.UserLoginInfo = userInfo;
+                    App.Token = tokenDetail;
+                    await Shell.Current.GoToAsync("//MainPage");
+                }
+            }
+
+        }
+    }
+}
