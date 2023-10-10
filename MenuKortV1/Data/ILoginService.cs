4340bfdfb7851cffbd5ce4a1e9ba9614de261d8d
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-10-10 13:18:30 +0200

    Create an interface for authentication system.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/Data/ILoginService.cs
@@ -0,0 +1,14 @@
+﻿using MenuKortV1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.Data
+{
+    public interface ILoginService
+    {
+        Task<LoginResponse> Authenticate(LoginRequest loginRequest);
+    }
+}
