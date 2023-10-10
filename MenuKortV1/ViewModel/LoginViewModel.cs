fce8f89807eda3d94858cb84b203133873171d7a
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-10-10 13:36:23 +0200

    Create viewmodel for the Login page. Check input and save login info.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

---
+++ MenuKortV1/ViewModel/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MenuKortV1.Data;
+using MenuKortV1.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuKortV1.ViewModel
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        //readonly ILoginService _loginService;
+        public LoginViewModel()
+        {
+            //_loginService = loginService;
+        }
+
+        [ObservableProperty]
+        string userName;
+
+        [ObservableProperty]
+        string password;
+
+        [RelayCommand]
+        async Task Login()
+        {
+            if(!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password)) 
+            {
+                ILoginService loginService = new LoginService();
+                var response = await loginService.Authenticate
+                ( 
+                    new LoginRequest
+                    {
+                        Username = UserName,
+                        Password = Password,
+                    }
+                );
+
+                if (response != null)
+                {
+                    response.UserLoginInfo = new UserLoginInfo
+                    {
+                        Username = UserName,
+                        RoleId = (int)UserLoginInfo.RoleDetails.Waiter,
+                        RoleName = "Tjener"
+                    };
+
+                    if (Preferences.ContainsKey(nameof(App.UserLoginInfo)))
+                    {
+                        Preferences.Remove(nameof(App.UserLoginInfo));
+                    }
+
+                    await SecureStorage.SetAsync(nameof(App.Token), response.Token);
+
+                    string userLoginInfo = JsonConvert.SerializeObject(response.UserLoginInfo);
+                    Preferences.Set(nameof(App.UserLoginInfo), userLoginInfo);
+                    App.UserLoginInfo = response.UserLoginInfo;
+                    App.Token = response.Token;
+                    await Shell.Current.GoToAsync("//MainPage");
+                }
+            }
+            else
+            {
+                await CustomCommands.AToastToYou("NOPE");
+            }
+        }
+    }
+}
