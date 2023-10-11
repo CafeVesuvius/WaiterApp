ab08df048b7a9e60f94ca47bf52579529da46e7b
TheRealScrumMasterIvana <ivan2346@edu.sde.dk>
2023-10-11 14:15:55 +0200

    Add notes.
    Signed-off-by: TheRealScrumMasterIvana <ivan2346@edu.sde.dk>

--- MenuKortV1/ViewModel/ActiveOrdersViewModel.cs
+++ MenuKortV1/ViewModel/ActiveOrdersViewModel.cs
@@ -18,39 +18,44 @@ namespace MenuKortV1.ViewModel
             Refresh();
         }
 
+        // Define OP counter for orders
         [ObservableProperty]
         int rowCounter;
 
+        // Define OP to store persistent data about the orders
         [ObservableProperty]
         PersistentOrder persistentOrder = new PersistentOrder();
 
+        // Define OP to store the list of orders in the preferences
         [ObservableProperty]
         List<Order> currentOrders = new List<Order>();
 
+        // Command to refresh the order list
         [RelayCommand]
         void Refresh()
         {
-            // Clear
+            // Overwrite existing data
             PersistentOrder = null;
             CurrentOrders.Clear();
-
-            // Get
             string checkPersistentOrderData = Preferences.Get(nameof(App.PersistentOrder), "");
             PersistentOrder currentPO = JsonConvert.DeserializeObject<PersistentOrder>(checkPersistentOrderData);
             PersistentOrder = currentPO;
             CurrentOrders = PersistentOrder.PersistentOrders.ToList();
 
+            // Show a counter the current orders for delivering
             RowCounter = CurrentOrders.Count;
         }
 
         [RelayCommand]
         void Remove(Order o)
         {
+            // Remove item from preferences
             string checkPersistentOrderData = Preferences.Get(nameof(App.PersistentOrder), "");
             PersistentOrder.PersistentOrders.Remove(o);
             string persistentOrderstStr = JsonConvert.SerializeObject(PersistentOrder);
             Preferences.Set(nameof(App.PersistentOrder), persistentOrderstStr);
 
+            // Auto overwrite
             Refresh();
         }
     }
